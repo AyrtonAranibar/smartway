3b239eda158f06e07b53988f5d617c7bda4e5abc
lWolfGangl <generalljhon@gmail.com>
2022-07-04 15:11:36 -0500

    mapa

--- app/Controllers/UsuariosController.php
+++ app/Controllers/UsuariosController.php
@@ -3,6 +3,8 @@ namespace App\Controllers;
 
 use CodeIgniter\Controller;
 use App\Models\Usuarios;
+use App\Models\ClientesModel;
+use App\Models\PedidosModel;
 use App\Controllers\Gmaps;
 use App\Libraries\Googlemaps;
 use App\Libraries\Dijkstra;
@@ -71,37 +73,8 @@ class UsuariosController extends Controller{
                 }
                 if($usuario['tipo_usuario']==3){
                 
-                    $google = new  \App\Libraries\Googlemaps();
-
-                    $config = array();
-                    $config['center'] = 'auto';
-                    $config['onboundschanged'] = 'if (!centreGot) {
-                    var mapCentre = map.getCenter();
-                    marker_0.setOptions({
-                    position: new google.maps.LatLng(mapCentre.lat(),      mapCentre.lng()) 
-                    });
-                    }
-                    centreGot = true;';
-            
-                    $config['center'] = 'udaipur, rajasthan, india';
-                    $config['zoom'] = 'auto';
-                    $config['directions'] = TRUE;
-                    $config['directionsMode'] = "DRIVING"; //modes: DRIVING WALKING BICYCLING
-                    $config['directionsStart'] = '-16.391250525, -71.542904013';
-                    $config['directionsWaypointArray'] = array(//7 decimales
-                    "-16.401342781446903, -71.55321621432907",
-                    "-16.375288483305315, -71.561128978274",
-                    "-16.351376699025202, -71.55806499666011",
-                    "-16.348994186542097, -71.54077233159111"
-                    );
-                    $config['directionsEnd'] = '-16.391250525, -71.542904013';
-                    $config['directionsDivID'] = 'directionsDiv';
-                    $google->initialize($config);
-                    $mapa['map'] = $google->create_map();
-            
-                    // $g = new  \App\Libraries\Graph();
-            
-            
+                
+                $mapa=$this->dibujarMapa();
 
                 return view('marker.php', $mapa);
                 }
@@ -129,7 +102,7 @@ class UsuariosController extends Controller{
     }
     function dibujarMapa(){
         $google = new  \App\Libraries\Googlemaps();
-
+        $posicion_local='-16.391250525, -71.542904013';
         $config = array();
         $config['center'] = 'auto';
         $config['onboundschanged'] = 'if (!centreGot) {
@@ -140,35 +113,67 @@ class UsuariosController extends Controller{
         }
         centreGot = true;';
 
-        $nodes = [['PUNTO 1','-16.3910877835','-71.54671308'],['PUNTO 2','-16.3743452','-71.5665099'],['PUNTO 3','-16.39889','-71.535'], ];
-
-        // $config['center'] = '-16.39889, -71.535';
-        $config['directionsStart'] = '-16.3910877835, -71.54671308';
-        $config['directionsEnd'] = '-16.3743452, -71.5665099';
+        $config['center'] = $posicion_local;
+        $config['zoom'] = 'auto';
+        $config['directions'] = TRUE;
+        $config['directionsMode'] = "DRIVING"; //modes: DRIVING WALKING BICYCLING
+        $config['directionsStart'] = $posicion_local;
+        $coordenadas = $this->generarArrayCoordenadas($posicion_local);
+        print_r($coordenadas);
+        $config['directionsWaypointArray'] = $coordenadas;
+        $config['directionsEnd'] = $posicion_local;
         $config['directionsDivID'] = 'directionsDiv';
-        // $config['zoom'] = 'auto';
         $google->initialize($config);
-        $marker = array();
-        $marker['position'] = '-16.39889, -71.535';
-        $google->add_marker($marker);
-        $data['map'] = $google->create_map();
+        $mapa['map'] = $google->create_map();
 
-        $g = new  \App\Libraries\Graph();
-
-
-        $g->addedge("PUNTO A", "PUNTO B", $this->getDistanceBetweenPointsNew($nodes[0][1], $nodes[0][2],$nodes[1][1], $nodes[1][2], 'kilometers'));
-        $g->addedge("PUNTO A", "PUNTO C", $this->getDistanceBetweenPointsNew($nodes[0][1], $nodes[0][2],$nodes[2][1], $nodes[2][2], 'kilometers'));
-        $g->addedge("PUNTO B", "PUNTO C", $this->getDistanceBetweenPointsNew($nodes[1][1], $nodes[1][2],$nodes[2][1], $nodes[2][2], 'kilometers'));
-        $g->addedge("PUNTO C", "PUNTO A", $this->getDistanceBetweenPointsNew($nodes[2][1], $nodes[2][2],$nodes[0][1], $nodes[0][2], 'kilometers'));
-
-
-        list($distances, $prev) = $g->paths_from("PUNTO A");
-        
-        $path = $g->paths_to($prev, "PUNTO C");
-        
-        print_r($path);
-
-        return $data;
+        return $mapa;
     }
-    
+
+    public function generarArrayCoordenadas($posicion_local){
+        $clientes = new ClientesModel();
+        $datos['clientes'] = $clientes->where('activo',1)->orderBy('id','ASC')->findAll();
+        $coordenadas = array();
+        $coordenadas_lat = array();
+        $coordenadas_lon = array();
+        foreach ($datos['clientes'] as $cliente){
+            //$coordenada_lat = floatval($cliente['latitud']);
+            //$coordenada_lon = floatval($cliente['longitud']);
+            $coordenada = $cliente['latitud'].', '.$cliente['longitud'];
+            array_push($coordenadas, $coordenada);
+        }
+        //$coordenadas= $this->generarCaminoMasCorto($coordenadas, $posicion_local);
+
+        return $coordenadas;
+    }
+
+
+    public function generarCaminoMasCorto($lista_cordenadas, $posicion_local){
+        $array_posicion_local = explode(',', $posicion_local);
+        $posicion_local_lat = floatval($array_posicion_local[0]);
+        $posicion_local_lon = floatval($array_posicion_local[1]);
+        $array_results=array();
+
+        
+        $minimo=0.0;
+        $index=0;
+        foreach ($lista_cordenadas as $key => $coordenada){
+            $resultado = $this->distanciaEntreDosPuntos($posicion_local_lat, $posicion_local_lon, $coordenada['latitud'], $coordenada['longitud']);
+            if ($key == 0){
+                $minimo = $resultado;
+                $index=0;
+            }else{
+                if ($minimo > $resultado){
+                    $minimo =  $resultado;
+                    $index=$key;
+                }
+            }
+        }
+        array_push($array_results, array($posicion_local_lat, $posicion_local_lon ));
+    }
+
+
+    public function distanciaEntreDosPuntos($lat1,$lon1,$lat2,$lon2){
+        $resultado=sqrt(($lat2 - $lat1)+($lon2 - $lon1));
+        return $resultado;
+    } 
 }
